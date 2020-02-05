76bc38056b90201e9ebc627d7d65f43cbe4b3fdc
Bastian Kistner <bastian.kistner@gmail.com>
2020-02-05 12:24:54 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,16 @@ set -e
 BRANCH=$(echo $GITHUB_REF | rev | cut -f 1 -d / | rev)
 LOCAL_IMAGE_NAME=${GITHUB_REPOSITORY}_${INPUT_WORKING_DIRECTORY}:${GITHUB_SHA}
 GCR_IMAGE_NAME=${INPUT_REGISTRY}/${INPUT_PROJECT}/${LOCAL_IMAGE_NAME}
-SERVICE_NAME=${INPUT_WORKING_DIRECTORY}_${BRANCH}
+SERVICE_NAME=${INPUT_SERVICE_NAME}_${BRANCH}
 
 echo "BRANCH = ${BRANCH}"
-echo 
+echo "LOCAL_IMAGE_NAME = ${LOCAL_IMAGE_NAME}"
+echo "GCR_IMAGE_NAME = ${GCR_IMAGE_NAME}"
+echo "SERVICE_NAME = ${SERVICE_NAME}"
 
 # service key
 
-echo "$INPUT_SERVICE_KEY" | base64 --decode > "$HOME"/gcloud.json
+echo "$INPUT_GCP_SERVICE_KEY" | base64 --decode > "$HOME"/gcloud.json
 
 # Prepare env vars if `env` is set to file 
 
@@ -28,15 +30,22 @@ fi
 
 # run 
 
+echo "\nActivate service account..."
 gcloud auth activate-service-account --key-file="$HOME"/gcloud.json --project "$INPUT_PROJECT"
-gcloud auth configure-docker
 
-cd $INPUT_WORKING_DIRECTORY
+echo "\nConfigure docker..."
+gcloud auth configure-docker --quiet
 
+cd ${GITHUB_WORKSPACE}/${INPUT_WORKING_DIRECTORY}
+
+echo "\nBuild image..."
 docker build -t ${LOCAL_IMAGE_NAME} .
+echo "\nTag image..."
 docker tag ${LOCAL_IMAGE_NAME} ${GCR_IMAGE_NAME}
+echo "\nPush image..."
 docker push "$GCR_IMAGE_NAME"
 
+echo "\nDeploy to cloud run..."
 gcloud beta run deploy "$SERVICE_NAME" \
   --image "$GCR_IMAGE_NAME" \
   --region "$INPUT_REGION" \
