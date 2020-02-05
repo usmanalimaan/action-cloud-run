c465b3d1333e3d6ec3035388148f90c767e8e585
Bastian Kistner <bastian.kistner@gmail.com>
2020-02-05 11:51:48 +0100

    changed implementation

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,16 @@
 
 set -e
 
+BRANCH=$(echo $GITHUB_REF | rev | cut -f 1 -d / | rev)
+LOCAL_IMAGE_NAME=${GITHUB_REPOSITORY}_${INPUT_WORKING_DIRECTORY}:${GITHUB_SHA}
+GCR_IMAGE_NAME=${INPUT_REGISTRY}/${INPUT_PROJECT}/${LOCAL_IMAGE_NAME}
+SERVICE_NAME=${INPUT_WORKING_DIRECTORY}_${BRANCH}
+
+# service key
+
 echo "$INPUT_SERVICE_KEY" | base64 --decode > "$HOME"/gcloud.json
+
+# Prepare env vars if `env` is set to file 
 
 if [ "$INPUT_ENV" ]
 then
@@ -13,16 +22,22 @@ if [ "$ENVS" ]
 then
     ENV_FLAG="--set-env-vars $ENVS"
 else
-    ENV_FLAG="--clear-env-vars"
+    # ENV_FLAG="--clear-env-vars"
 fi
+
+# run 
 
 gcloud auth activate-service-account --key-file="$HOME"/gcloud.json --project "$INPUT_PROJECT"
 gcloud auth configure-docker
 
-docker push "$INPUT_IMAGE"
+cd $INPUT_WORKING_DIRECTORY
 
-gcloud beta run deploy "$INPUT_SERVICE" \
-  --image "$INPUT_IMAGE" \
+docker build -t ${LOCAL_IMAGE_NAME} .
+docker tag ${LOCAL_IMAGE_NAME} ${GCR_IMAGE_NAME}
+docker push "$GCR_IMAGE_NAME"
+
+gcloud beta run deploy "$SERVICE_NAME" \
+  --image "$GCR_IMAGE_NAME" \
   --region "$INPUT_REGION" \
   --platform managed \
   --allow-unauthenticated \
