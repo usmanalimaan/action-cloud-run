56a7610cb0d9ed5d1a5f4f993ca24513dad86ee5
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2020-02-22 14:52:29 +0100

    transform repo name to all lowercase

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,8 @@ fi
 set -e
 
 BRANCH=$(echo $GITHUB_REF | rev | cut -f 1 -d / | rev)
-GCR_IMAGE_NAME=${INPUT_REGISTRY}/${INPUT_PROJECT}/${GITHUB_REPOSITORY}${IMAGE_POSTFIX}
+REPO=$(echo $GITHUB_REPOSITORY | tr '[:upper:]' '[:lower:]')
+GCR_IMAGE_NAME=${INPUT_REGISTRY}/${INPUT_PROJECT}/${REPO}${IMAGE_POSTFIX}
 SERVICE_NAME=${INPUT_SERVICE_NAME}--${BRANCH}
 
 echo "\n\n-----------------------------------------------------------------------------\n\n"
